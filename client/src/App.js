cd91993b30b2e18a763eb28d046b562423dcfe22
Carlos G <carlosjr3000@hotmail.com>
2018-06-26 21:37:54 -0500

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Welcome to Camibook</h1>
+        <h1>Welcome to Camibook11</h1>
       </div>
     );
   }
